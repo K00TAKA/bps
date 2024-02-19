e7832497565ebacf924c0a630bbd17f70c63f359
K00TAKA <147153187+K00TAKA@users.noreply.github.com>
2024-02-19 02:57:02 +0000

    [start]複数権限deviseの登録・修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    if member_signed_in?
+      __________path
+    elsif admin_signed_in?
+      __________path
+    else
+      root_path
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    if resource == :admin
+      __________path
+    else
+      root_path
+    end
+  end
+
 end
