b9158424bcbfc50428c29408eb58101a146d66c1
K00TAKA <147153187+K00TAKA@users.noreply.github.com>
2024-02-21 05:54:03 +0000

    [add view/controller]top 新着 anouncement company

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,11 @@
 class HomesController < ApplicationController
+
   def top
+    @companies = Company.order(created_at: :desc).limit(5)
+    @announcements = Announcement.order(created_at: :desc).limit(5)
   end
+
+  def about
+  end
+
 end
