a6ecb02a186d782b400746e2a943bd8c360d89a5
K00TAKA <147153187+K00TAKA@users.noreply.github.com>
2024-02-21 05:25:58 +0000

    [start]comment投稿

--- app/controllers/member/comments_controller.rb
+++ app/controllers/member/comments_controller.rb
@@ -2,14 +2,20 @@ class Member::CommentsController < ApplicationController
 
   before_action :authenticate_member!
 
+  def new
+    @comment = Comment.new
+  end
+
   def create
+    @member = current_member
     @comment = Comment.new(comment_params)
+    @comment.member_id = current_member_id
     if comment.save
       flash[:notice] = "コメントの登録に成功しました。"
-      redirect_to __________path
+      redirect_to company_path
     else
       flash[:notice] = "コメントの登録に失敗しました。"
-      render :__________
+      render :index
     end
   end
 
