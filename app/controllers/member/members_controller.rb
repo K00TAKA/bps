afb531aa85f7c3666750bd15287c602441293bda
K00TAKA <147153187+K00TAKA@users.noreply.github.com>
2024-02-27 14:00:21 +0000

    [add]mypage

--- app/controllers/member/members_controller.rb
+++ app/controllers/member/members_controller.rb
@@ -1,2 +1,31 @@
 class Member::MembersController < ApplicationController
+
+  before_action :authenticate_member!
+
+  def show
+    @member = Member.find(params[:id])
+    @company = Member.company_id
+  end
+
+  def edit
+    @member = Member.find(params[:id])
+  end
+
+  def update
+    @member = Member.find([:id])
+    if @member.update(member_params)
+      flash[:notice] = "会員情報更新に成功しました"
+      redirect_to edit_member_path(params[:id])
+    else
+      flash[:notice] = "会員情報更新に失敗しました"
+      render :edit
+    end
+  end
+
+  private
+
+  def member_params
+  params.require(:member).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :email)
+  end
+
 end
