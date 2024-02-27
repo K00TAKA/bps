afe75858fd81eb3b97c8e7c5af40f3122b636259
K00TAKA <147153187+K00TAKA@users.noreply.github.com>
2024-02-27 05:55:57 +0000

    [add]admin 会員管理

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -6,4 +6,21 @@ class Admin::MembersController < ApplicationController
     @members = Member.all
   end
 
+  def update
+    @member = Member.find(params[:id])
+    if @member.update(member_params)
+      flash[:notice] = "会員ステータスを更新しました"
+      redirect_to admin_member_path(@member)
+    else
+      flash[:notice] = "会員ステータスの更新に失敗しました"
+      render :index
+    end
+  end
+
+  private
+
+  def member_params
+    params.require(:member).permit(:is_active)
+  end
+
 end
