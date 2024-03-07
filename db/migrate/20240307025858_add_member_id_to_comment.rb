5ba40ea0d1698866f5c65d19b453098c2efa2549
K00TAKA <147153187+K00TAKA@users.noreply.github.com>
2024-03-07 04:59:05 +0000

    [fix]comment model

---
+++ db/migrate/20240307025858_add_member_id_to_comment.rb
@@ -0,0 +1,5 @@
+class AddMemberIdToComment < ActiveRecord::Migration[6.1]
+  def change
+    add_column :comments, :member_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_03_05_114212) do
+ActiveRecord::Schema.define(version: 2024_03_07_030136) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -61,6 +61,8 @@ ActiveRecord::Schema.define(version: 2024_03_05_114212) do
     t.boolean "is_active", default: true, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "member_id"
+    t.integer "company_id"
   end
 
   create_table "companies", force: :cascade do |t|
