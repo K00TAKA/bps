f382073dc8e70f78c32aaf163f02dcaa48cfaeb3
K00TAKA <147153187+K00TAKA@users.noreply.github.com>
2024-03-12 05:06:42 +0000

    [fix]announcement

--- app/controllers/admin/announcements_controller.rb
+++ app/controllers/admin/announcements_controller.rb
@@ -27,17 +27,16 @@ class Admin::AnnouncementsController < ApplicationController
 
   def edit
     @announcement = Announcement.find(params[:id])
-    @admin = current_admin
   end
 
   def update
-    @admin = current_admin
     @announcement = Announcement.find(params[:id])
 
-
-    if @announcement.save
-      redirect_to
+    if @announcement.update(announcement_params)
+      flash[:notice] = "更新に成功しました。"
+      redirect_to admin_announcements_path
     else
+      flash[:notice] = "更新に成功しました。"
       render :edit
     end
   end
