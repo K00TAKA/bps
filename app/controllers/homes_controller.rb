0c4f83fecb252d5b4ab16a1561cf0c87ea2e0aac
K00TAKA <147153187+K00TAKA@users.noreply.github.com>
2024-03-31 05:15:20 +0000

    [fix]homes company disp limit

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,7 +2,9 @@ class HomesController < ApplicationController
 
   def top
     #新着5件表示する
-    @companies = Company.order(created_at: :desc).limit(5)
+    @companies = Company.where(is_active: true).order(created_at: :desc).limit(5)
+    # if company.is_active?
+    #   Company..limit(5)
     @announcements = Announcement.order(created_at: :desc).limit(5)
   end
 
