22a5ec02a760f5dd945eb1dbbb2329f9d7959e22
K00TAKA <147153187+K00TAKA@users.noreply.github.com>
2024-04-23 06:43:55 +0000

    [back]favorite

--- app/controllers/member/favorites_controller.rb
+++ app/controllers/member/favorites_controller.rb
@@ -6,14 +6,14 @@ class Member::FavoritesController < ApplicationController
     comment = Comment.find(params[:comment_id])
     favorite = current_member.favorites.new(comment_id: comment.id)
     favorite.save
-    # redirect_to request.referer
+    redirect_to request.referer
   end
 
   def destroy
     comment = Comment.find(params[:comment_id])
     favorite = current_member.favorites.find_by(comment_id: comment.id)
     favorite.destroy
-    # redirect_to request.referer
+    redirect_to request.referer
   end
 
 end
