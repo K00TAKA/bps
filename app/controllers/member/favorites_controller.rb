e36c0e01efb4f35f8e4ee4d7aabb4a8a411baaf5
K00TAKA <147153187+K00TAKA@users.noreply.github.com>
2024-04-28 12:51:11 +0000

    [add]favorite Ajax

--- app/controllers/member/favorites_controller.rb
+++ app/controllers/member/favorites_controller.rb
@@ -3,17 +3,17 @@ class Member::FavoritesController < ApplicationController
   before_action :authenticate_member!
 
   def create
-    comment = Comment.find(params[:comment_id])
-    favorite = current_member.favorites.new(comment_id: comment.id)
+    @comment = Comment.find(params[:comment_id])
+    favorite = current_member.favorites.new(comment_id: @comment.id)
     favorite.save
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   def destroy
-    comment = Comment.find(params[:comment_id])
-    favorite = current_member.favorites.find_by(comment_id: comment.id)
+    @comment = Comment.find(params[:comment_id])
+    favorite = current_member.favorites.find_by(comment_id: @comment.id)
     favorite.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
 end
