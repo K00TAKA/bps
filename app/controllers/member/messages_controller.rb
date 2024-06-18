4cd9d593786b00334db17048779ea5deb18a0885
K00TAKA <147153187+K00TAKA@users.noreply.github.com>
2024-06-18 05:51:10 +0000

    [fix]messages_controller

--- app/controllers/member/messages_controller.rb
+++ app/controllers/member/messages_controller.rb
@@ -8,22 +8,9 @@ class Member::MessagesController < ApplicationController
     if Entry.where(member_id: current_member.id, room_id: @room.id).present?
       @message = Message.new(message_params)
       if @message.save
-        @roommembernotme = Entry.where(room_id: @room.id).where.not(member_id: current_member.id)
-        @theid = @roommembernotme.find_by(room_id: @room.id)
-        if @theid
-          notification = current_member.active_notifications.new(
-            room_id: @room.id,
-            message_id: @message.id,
-            visited_id: @theid.member_id,
-            visitor_id: current_member.id,
-            action: 'message'
-          )
-          notification.checked = true if notification.visitor_id == notification.visited_id
-          notification.save if notification.valid?
-        end
         flash[:notice] = "送信しました。"
       else
-        Rails.logger.error(@message.errors.full_messages.join(', ')) # Add this line to log the error messages
+        Rails.logger.error(@message.errors.full_messages.join(', '))
         flash[:notice] = "送信に失敗しました。"
       end
     end
